3de68795466d05796eb962c06fb2129ce57e02e6
choizz156 <106965005+choizz156@users.noreply.github.com>
2022-07-24 19:05:34 +0900

    필요없는 클래스

--- TRPG(수정)/User2.java
+++
@@ -1,9 +0,0 @@
-public class User2 extends Users {
-    String[] user2Info = new String[4];
-
-    public void infom() {
-        user2Info = userCreate();
-        userPrint(user2Info);
-    }
-}
-
